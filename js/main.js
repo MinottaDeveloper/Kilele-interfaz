88b8d5866914f5b8fd26466158a5aa46b9f0e2f1
MinottaDeveloper <42478783+MinottaDeveloper@users.noreply.github.com>
2020-05-14 22:38:32 -0500

    PreTerminadoJM

--- js/main.js
+++ js/main.js
@@ -7,10 +7,24 @@ const btn_comprarCapuchino = document.querySelector('#btn_comprar_producto_capuc
 const btn_comprarAmericano = document.querySelector('#btn_comprar_producto_americano');
 const btn_comprarMocca = document.querySelector('#btn_comprar_producto_mocca');
 const btn_comprarEspresso = document.querySelector('#btn_comprar_producto_espreso');
+const btn_continuarCompra = document.querySelector('.coffee__container__comprar');
+const btn_confirmarPedidoCaracteristicas = document.querySelector('.btn_confirmarPedidoCaracteristicas');
+const btn_tarjeta_credito = document.querySelector('.tarjeta_credito');
+const btn_pagarEfectivo = document.querySelector('.efectivo');
+const btn_comprar_efectivo = document.querySelector('.btn_comprar_efectivo');
+const btn_comprar_tarjeta = document.querySelector('.btn_comprar_tarjeta');
+const btn_otraBebida = document.querySelector('.btn_otraBebida');
+const btn_finalizarPedido = document.querySelector('.btn_finalizarPedido');
+
 
 const pantalla1 = document.querySelector('.pantalla_1');
 const pantalla2 = document.querySelector('.pantalla_2');
 const pantalla3 = document.querySelector('.pantalla_3');
+const pantalla4 = document.querySelector('.pantalla_4');
+const pantalla5 = document.querySelector('.pantalla_5');
+const pantalla_pagoEfectivo = document.querySelector('.pantalla_pagoEfectivo');
+const pantalla_pagoTarjeta = document.querySelector('.pantalla_pagoTarjeta');
+const pantalla_final = document.querySelector('.pantalla_final');
 
 var imagenProductoDescripcion = document.querySelector('.imagenProductoDesc');
 var nombreProductoDesc = document.querySelector('.nombreProductoDesc');
@@ -69,6 +83,75 @@ function comprarEspresso(){
   console.log(productoSeleccionado);
 }
 
+function continuarCompra(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'none';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'flex';
+}
+
+function confirmarCompraOrigen(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'none';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'none';
+pantalla5.style.display = 'flex';
+}
+
+function mostrarPantallaTarjeta(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'none';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'none';
+pantalla5.style.display = 'none';
+pantalla_pagoTarjeta.style.display = 'flex';
+}
+
+function mostrarPantallaEfectivo(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'none';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'none';
+pantalla5.style.display = 'none';
+pantalla_pagoTarjeta.style.display = 'none';
+pantalla_pagoEfectivo.style.display = 'flex';
+}
+
+function mostrarPantallaFinal(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'none';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'none';
+pantalla5.style.display = 'none';
+pantalla_pagoTarjeta.style.display = 'none';
+pantalla_pagoEfectivo.style.display = 'none';
+pantalla_final.style.display = 'flex';
+}
+
+function comprarOtraBebida(){
+pantalla1.style.display = 'none';
+pantalla2.style.display = 'flex';
+pantalla3.style.display = 'none';
+pantalla4.style.display = 'none';
+pantalla5.style.display = 'none';
+pantalla_pagoTarjeta.style.display = 'none';
+pantalla_pagoEfectivo.style.display = 'none';
+pantalla_final.style.display = 'none';
+}
+
+function finalizarPedido(){
+  pantalla1.style.display = 'flex';
+  pantalla2.style.display = 'none';
+  pantalla3.style.display = 'none';
+  pantalla4.style.display = 'none';
+  pantalla5.style.display = 'none';
+  pantalla_pagoTarjeta.style.display = 'none';
+  pantalla_pagoEfectivo.style.display = 'none';
+  pantalla_final.style.display = 'none';
+}
+
+
+
 
 btn_comprar.addEventListener('click', procesoCompra);
 
@@ -76,5 +159,18 @@ btn_comprarCapuchino.addEventListener('click', comprarCapuchino);
 btn_comprarAmericano.addEventListener('click', comprarAmericano);
 btn_comprarMocca.addEventListener('click', comprarMocca);
 btn_comprarEspresso.addEventListener('click', comprarEspresso);
+
+btn_continuarCompra.addEventListener('click', continuarCompra);
+btn_confirmarPedidoCaracteristicas.addEventListener('click', confirmarCompraOrigen);
+
+btn_tarjeta_credito.addEventListener('click', mostrarPantallaTarjeta);
+btn_pagarEfectivo.addEventListener('click', mostrarPantallaEfectivo);
+
+btn_comprar_efectivo.addEventListener('click', mostrarPantallaFinal);
+btn_comprar_tarjeta.addEventListener('click', mostrarPantallaFinal);
+
+btn_otraBebida.addEventListener('click', comprarOtraBebida);
+btn_finalizarPedido.addEventListener('click', finalizarPedido);
+
 
 });
